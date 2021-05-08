e248fb3b4fcfe0b105cddb5125442f768abb43fe
bkmceh <IfhnlbyjdBkmceh@yandex.ru>
2021-05-08 19:17:55 +0300

    add ManyArithmeticOperationsException

---
+++ src/javamentor/exceptions/ManyArithmeticOperationsException.java
@@ -0,0 +1,8 @@
+package javamentor.exceptions;
+
+public class ManyArithmeticOperationsException extends RuntimeException {
+
+    public ManyArithmeticOperationsException(String message) {
+        super(message);
+    }
+}
