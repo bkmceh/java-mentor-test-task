c26f7277e75236a7b0824d51a2a6970dd6048e57
bkmceh <IfhnlbyjdBkmceh@yandex.ru>
2021-05-08 19:18:16 +0300

    add NotFoundArithmeticOperationException

---
+++ src/javamentor/exceptions/NotFoundArithmeticOperationException.java
@@ -0,0 +1,8 @@
+package javamentor.exceptions;
+
+public class NotFoundArithmeticOperationException extends RuntimeException {
+
+    public NotFoundArithmeticOperationException(String message) {
+        super(message);
+    }
+}
