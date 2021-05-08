116c97743d451b883c05ad8e2cbb48f89b86926e
bkmceh <IfhnlbyjdBkmceh@yandex.ru>
2021-05-09 01:27:12 +0300

    add new class Operation

---
+++ src/javamentor/validation/Operation.java
@@ -0,0 +1,24 @@
+package javamentor.validation;
+
+public class Operation {
+
+    private boolean isOneOperation;
+
+    private int operationIndex;
+
+    public boolean isOneOperation() {
+        return isOneOperation;
+    }
+
+    public void setOneOperation(boolean oneOperation) {
+        isOneOperation = oneOperation;
+    }
+
+    public int getOperationIndex() {
+        return operationIndex;
+    }
+
+    public void setOperationIndex(int operationIndex) {
+        this.operationIndex = operationIndex;
+    }
+}
