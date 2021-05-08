b9b30df4b3899e58ab7ff06daf16719b47aa37a1
bkmceh <IfhnlbyjdBkmceh@yandex.ru>
2021-05-08 19:19:13 +0300

    change Main

--- src/javamentor/Main.java
+++ src/javamentor/Main.java
@@ -1,8 +1,18 @@
 package javamentor;
 
+import javamentor.validation.StringValidation;
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+
+        String inputString = scanner.nextLine();
+
+        if (!StringValidation.validate(inputString)) {
+            System.out.println("Not found((((");
+        }
     }
 }
