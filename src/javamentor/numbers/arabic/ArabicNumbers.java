14c2f0065fa0a3b8c86ebeb4a5bbf58bb6ecda11
bkmceh <IfhnlbyjdBkmceh@yandex.ru>
2021-05-09 23:06:27 +0300

    create ArabicNumbers.java to store 2 numbers in Arabic format

---
+++ src/javamentor/numbers/arabic/ArabicNumbers.java
@@ -0,0 +1,24 @@
+package javamentor.numbers.arabic;
+
+public class ArabicNumbers {
+
+    private static int firstNumber;
+
+    private static int secondNumber;
+
+    public static int getFirstNumber() {
+        return firstNumber;
+    }
+
+    public static void setFirstNumber(int firstNumber) {
+        ArabicNumbers.firstNumber = firstNumber;
+    }
+
+    public static int getSecondNumber() {
+        return secondNumber;
+    }
+
+    public static void setSecondNumber(int secondNumber) {
+        ArabicNumbers.secondNumber = secondNumber;
+    }
+}
