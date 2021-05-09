e97ec77d71d66fecb9793ba9c9b1233a7ac74074
bkmceh <IfhnlbyjdBkmceh@yandex.ru>
2021-05-09 23:03:55 +0300

    change StringValidation.java

--- src/javamentor/validation/StringValidation.java
+++ src/javamentor/validation/StringValidation.java
@@ -4,7 +4,11 @@ import javamentor.exceptions.ManyArithmeticOperationsException;
 import javamentor.exceptions.NotFoundArithmeticOperationException;
 import javamentor.exceptions.RomanNumbersException;
 import javamentor.exceptions.SpaceExceptions;
-import javamentor.validation.numbers.RomanNumbers;
+import javamentor.numbers.Numbers;
+import javamentor.numbers.arabic.ArabicNumbers;
+import javamentor.numbers.roman.RomanConverter;
+import javamentor.numbers.roman.RomanExceptionsChecker;
+import javamentor.numbers.roman.RomanNumbersEnum;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -13,31 +17,27 @@ public class StringValidation {
 
     private static final Pattern pattern = Pattern.compile("[+\\-*/]");
 
-
     public static void validate(String inputString) {
 
-        Operation operation;
-        operation = checkOperations(inputString);
+        checkOperations(inputString);
 
-        checkSpaces(inputString, operation);
+        System.out.println(Operation.getOperationSign());
 
-        checkNumbers(inputString, operation);
+        checkSpaces(inputString);
 
+        checkNumbers(inputString);
 
     }
 
-    private static Operation checkOperations(final String inputString) {
-
-        Operation operation = new Operation();
+    private static void checkOperations(final String inputString) {
 
         int count = 0;
         Matcher matcher = pattern.matcher(inputString);
 
         while (matcher.find()) {
             count++;
-            operation.setOperationIndex(matcher.start());
+            Operation.setOperationIndex(matcher.start());
         }
-        System.out.format("Совпадений %s\n", count);
 
         if (count > 1) {
             throw new ManyArithmeticOperationsException("Число арифметических операций чет много");
@@ -46,36 +46,50 @@ public class StringValidation {
             throw new NotFoundArithmeticOperationException("Не найдена арифметическая операция.");
         }
 
-        return operation;
+        Operation.setOperationSign(inputString.charAt(Operation.getOperationIndex()));
+
     }
 
-    private static void checkSpaces(final String inputString, final Operation operation) {
+    private static void checkSpaces(final String inputString) {
         if (inputString.indexOf(' ') == 0
                 || inputString.lastIndexOf(' ') == inputString.length() - 1) {
             throw new SpaceExceptions("Проверьте лишние пробелы в начале или в конце и введенные числа.");
         }
 
-        if (operation.getOperationIndex() - inputString.indexOf(' ') != 1) {
+        if (Operation.getOperationIndex() - inputString.indexOf(' ') != 1) {
             throw new SpaceExceptions("Проверьте расположение арифметического знака.");
         }
 
-        if (inputString.lastIndexOf(' ') - operation.getOperationIndex() != 1) {
+        if (inputString.lastIndexOf(' ') - Operation.getOperationIndex() != 1) {
             throw new SpaceExceptions("Проверьте расположение арифметического знака.");
         }
     }
 
-    private static void checkNumbers(final String inputString, final Operation operation) {
+    private static void checkNumbers(final String inputString) {
+
+        String firstNumber = inputString.substring(0, Operation.getOperationIndex() - 1);
+        String secondNumber = inputString.substring(Operation.getOperationIndex() + 2);
+
+        if (isRomanNumbers(firstNumber, secondNumber)) {
+            Numbers.isRoman = true;
+            System.out.println("This numbers are roman!");
+            RomanConverter.convertRomanToArabic();
+            ArabicNumbers.setFirstNumber(RomanConverter.romanToArabic.get(firstNumber));
+            ArabicNumbers.setSecondNumber(RomanConverter.romanToArabic.get(secondNumber));
+            RomanExceptionsChecker.checkOnExceptions(
+                    ArabicNumbers.getFirstNumber(),
+                    ArabicNumbers.getSecondNumber());
+        }
+
+    }
+
+    private static boolean isRomanNumbers(final String firstNumber,
+                                          final String secondNumber) {
 
         int romanCount = 0;
-        int arabicCount = 0;
 
-        String firstNumber = inputString.substring(0, operation.getOperationIndex() - 1);
-        String secondNumber = inputString.substring(operation.getOperationIndex() + 2);
-
-        System.out.format("\nFirst: %s\nSecond: %s\n", firstNumber, secondNumber);
-
-        for (RomanNumbers number:
-             RomanNumbers.values()) {
+        for (RomanNumbersEnum number :
+                RomanNumbersEnum.values()) {
             if (firstNumber.equals(number.toString())) {
                 romanCount++;
             }
@@ -84,11 +98,11 @@ public class StringValidation {
             }
         }
 
-        System.out.format("\nCount of Roman numbers: %s\n", romanCount);
-
         if (romanCount == 1) {
             throw new RomanNumbersException("Римские числа должны быть от 1 до 10. " +
                     "Оба числа должны быть римскими.");
         }
+
+        return romanCount == 2;
     }
 }
