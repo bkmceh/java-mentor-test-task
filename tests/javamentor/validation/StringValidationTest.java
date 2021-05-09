118cfe4ef5e013f8d64038be69a2e70f2f8a7755
bkmceh <IfhnlbyjdBkmceh@yandex.ru>
2021-05-09 15:22:38 +0300

    created tests for checking StringValidation class

---
+++ tests/javamentor/validation/StringValidationTest.java
@@ -0,0 +1,107 @@
+package javamentor.validation;
+
+import javamentor.exceptions.ManyArithmeticOperationsException;
+import javamentor.exceptions.NotFoundArithmeticOperationException;
+import javamentor.exceptions.SpaceExceptions;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StringValidationTest {
+
+    @Test(expected = NotFoundArithmeticOperationException.class)
+    public void NotFoundOperations1() {
+        String inputString = " 5454";
+        StringValidation.validate(inputString);
+    }
+
+    @Test(expected = NotFoundArithmeticOperationException.class)
+    public void NotFoundOperations2() {
+        String inputString = "";
+        StringValidation.validate(inputString);
+    }
+
+    @Test(expected = ManyArithmeticOperationsException.class)
+    public void TooManyOperations1() {
+        String inputString = "4 + 5 - 4";
+        StringValidation.validate(inputString);
+    }
+
+    @Test(expected = ManyArithmeticOperationsException.class)
+    public void TooManyOperations2() {
+        String inputString = "+-";
+        StringValidation.validate(inputString);
+    }
+
+    @Test(expected = SpaceExceptions.class)
+    public void SpacesFromBegin1() {
+        String inputString = "    5 + 34";
+        StringValidation.validate(inputString);
+    }
+
+    @Test(expected = SpaceExceptions.class)
+    public void SpacesFromBegin2() {
+        String inputString = " 5+34";
+        StringValidation.validate(inputString);
+    }
+
+    @Test(expected = SpaceExceptions.class)
+    public void SpacesFromEnd1() {
+        String inputString = "5 + 34      ";
+        StringValidation.validate(inputString);
+    }
+
+    @Test(expected = SpaceExceptions.class)
+    public void SpacesFromEnd2() {
+        String inputString = "5 + 34 ";
+        StringValidation.validate(inputString);
+    }
+
+    @Test(expected = SpaceExceptions.class)
+    public void SpacesFromBeginAndEnd() {
+        String inputString = "   5 + 34  ";
+        StringValidation.validate(inputString);
+    }
+
+    @Test(expected = SpaceExceptions.class)
+    public void checkingSpaces1() {
+        String inputString = "5 +34";
+        StringValidation.validate(inputString);
+    }
+
+    @Test(expected = SpaceExceptions.class)
+    public void checkingSpaces2() {
+        String inputString = "5+34";
+        StringValidation.validate(inputString);
+    }
+
+    @Test(expected = SpaceExceptions.class)
+    public void checkingSpaces3() {
+        String inputString = "+534";
+        StringValidation.validate(inputString);
+    }
+
+    @Test(expected = SpaceExceptions.class)
+    public void checkingSpaces4() {
+        String inputString = "5   +   34";
+        StringValidation.validate(inputString);
+    }
+
+    @Test(expected = SpaceExceptions.class)
+    public void checkingSpaces5() {
+        String inputString = " + 34";
+        StringValidation.validate(inputString);
+    }
+
+    @Test(expected = SpaceExceptions.class)
+    public void checkingSpaces6() {
+        String inputString = " + ";
+        StringValidation.validate(inputString);
+    }
+
+    @Test(expected = SpaceExceptions.class)
+    public void checkingSpaces7() {
+        String inputString = "5+ 4";
+        StringValidation.validate(inputString);
+    }
+}
