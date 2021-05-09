3918bffcc3b2ad66bc1e92ab78a6cdea3c3e4ce5
bkmceh <IfhnlbyjdBkmceh@yandex.ru>
2021-05-09 15:20:28 +0300

    create Calculation class

---
+++ src/javamentor/calculation/Calculation.java
@@ -0,0 +1,12 @@
+package javamentor.calculation;
+
+import javamentor.validation.StringValidation;
+
+public class Calculation {
+
+    public static void calculate(final String inputString) {
+
+        StringValidation.validate(inputString);
+
+    }
+}
