f9f1288852f0e9a5a956e7d0944381caf0fe1239
bkmceh <IfhnlbyjdBkmceh@yandex.ru>
2021-05-10 22:55:29 +0300

    create ArabicNumbersException.java

---
+++ src/javamentor/exceptions/ArabicNumbersException.java
@@ -0,0 +1,7 @@
+package javamentor.exceptions;
+
+public class ArabicNumbersException extends RuntimeException {
+    public ArabicNumbersException(final String message) {
+        super(message);
+    }
+}
