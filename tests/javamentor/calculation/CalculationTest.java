b3d4fb304bc64ece21efc84ca6aa8d4b9c91c692
bkmceh <IfhnlbyjdBkmceh@yandex.ru>
2021-05-10 22:56:48 +0300

    create new tests

--- tests/javamentor/calculation/CalculationTest.java
+++ tests/javamentor/calculation/CalculationTest.java
@@ -1,5 +1,6 @@
 package javamentor.calculation;
 
+import javamentor.exceptions.ArabicNumbersException;
 import javamentor.exceptions.RomanNumbersException;
 import javamentor.validation.StringValidation;
 import org.junit.Test;
@@ -35,6 +36,30 @@ public class CalculationTest {
     @Test(expected = RomanNumbersException.class)
     public void RomanNumbers4() {
         String inputString = "X / IV";
+
+        StringValidation.validate(inputString);
+        Calculation.calculate();
+    }
+
+    @Test
+    public void ArabicNumbers1() {
+        String inputString = "1 - 6";
+
+        StringValidation.validate(inputString);
+        Calculation.calculate();
+    }
+
+    @Test
+    public void ArabicNumbers2() {
+        String inputString = "5 * 10";
+
+        StringValidation.validate(inputString);
+        Calculation.calculate();
+    }
+
+    @Test(expected = ArabicNumbersException.class)
+    public void ArabicNumbers3() {
+        String inputString = "13 - 6";
 
         StringValidation.validate(inputString);
         Calculation.calculate();
