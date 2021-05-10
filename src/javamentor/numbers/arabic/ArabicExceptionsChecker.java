1abdb0b412e5c0227a3e2c684b80187a96d8760a
bkmceh <IfhnlbyjdBkmceh@yandex.ru>
2021-05-10 22:54:53 +0300

    create ArabicExceptionsChecker.java

---
+++ src/javamentor/numbers/arabic/ArabicExceptionsChecker.java
@@ -0,0 +1,19 @@
+package javamentor.numbers.arabic;
+
+import javamentor.exceptions.ArabicNumbersException;
+import javamentor.operation.Operation;
+
+public class ArabicExceptionsChecker {
+
+    public static void checkOnExceptions(final int firstNumber, final int secondNumber) {
+
+        if (firstNumber == 0 || firstNumber > 10 || secondNumber == 0 || secondNumber > 10) {
+            throw new ArabicNumbersException("Арабские числа должны быть от 1 до 10.");
+        }
+
+        if (firstNumber % secondNumber != 0 && Operation.getOperationSign() == '/') {
+            throw new ArabicNumbersException("Ответ при делении не целое число.");
+        }
+
+    }
+}
